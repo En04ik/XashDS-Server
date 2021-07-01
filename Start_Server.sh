982602c3caea547e88562624438ecc3373c7e8b3
En04ik <82300094+En04ik@users.noreply.github.com>
2021-07-01 14:13:25 +0600

    Update Start_Server.sh

--- Start_Server.sh
+++ Start_Server.sh
@@ -1,4 +1,4 @@
-echo "Starting server..."
+echo "Server started! Name of your server is Termux Server."
 cd ~/xash
 ./xash +deathmatch 1 +public 1 +maxplayers 8 +defaultmap crossfire +hostname Termux Server
-echo "Server started! Name of your server is Termux Server."
+echo "Server is killed."
