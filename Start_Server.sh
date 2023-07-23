40022529893afdad2687eaab36bb6aaa9ff7026b
qq430950189 <98295593+qq430950189@users.noreply.github.com>
2023-07-23 16:50:19 +0800

    Update Start_Server.sh
    my termux was applied to "cd xash"
    But i found it  Difficult toget termux with the permission to my new edited file( The file is  oporated with android file manager)

--- Start_Server.sh
+++ Start_Server.sh
@@ -1,4 +1,4 @@
 echo "Server started! Name of your server is Termux Server."
-cd ~/xash
+cd xash
 ./xash +deathmatch 1 +public 1 +maxplayers 8 +defaultmap crossfire +hostname Termux Server
 echo "Server is killed."
